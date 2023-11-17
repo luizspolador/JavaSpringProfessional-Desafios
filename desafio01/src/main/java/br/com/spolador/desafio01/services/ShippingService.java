d4b4157d186a5a836c68eebe76d359382b47620b
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2023-11-17 16:51:32 -0300

    desafio 1 concluido

---
+++ desafio01/src/main/java/br/com/spolador/desafio01/services/ShippingService.java
@@ -0,0 +1,17 @@
+package br.com.spolador.desafio01.services;
+
+import br.com.spolador.desafio01.entities.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingService {
+    public double shipment(Order order){
+        if(order.getBasic() < 100.00){
+            return 20.00;
+        } else if (order.getBasic() >= 100.00 && order.getBasic() < 200.00){
+            return 12.00;
+        } else {
+            return 0.00;
+        }
+    }
+}
