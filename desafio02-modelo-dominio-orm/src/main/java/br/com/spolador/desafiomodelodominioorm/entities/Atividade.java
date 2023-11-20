e1c022d9d8c96394c876b57bdbdbcbd3cbbe712e
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2023-11-20 11:23:17 -0300

    desafio02

---
+++ desafio02-modelo-dominio-orm/src/main/java/br/com/spolador/desafiomodelodominioorm/entities/Atividade.java
@@ -0,0 +1,30 @@
+package br.com.spolador.desafiomodelodominioorm.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "atividades")
+    private Set<Participante> participantes = new HashSet<>();
+}
+
